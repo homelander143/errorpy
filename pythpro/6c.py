205af5f62c76c0644dda723d71ca47509030749b
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:22:12 +0530

    Delete pythpro/6c.py

--- pythpro/6c.py
+++
@@ -1,9 +0,0 @@
-n=input("Enter the number : ")
-x=int(n)
-summ=0
-for i in n:
-    summ=summ+(int(i)**3)
-if summ==x:
-    print(x," is an armstrong number")
-else:
-    print(x," is not an armstrong number")
