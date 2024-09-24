4f55e267698d572b1c79ab3d25191160bf851469
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:16:03 +0530

    Delete pythpro/9c.py

--- pythpro/9c.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the value of n : "))
-m=int(input("Enter the value of m : "))
-for i in range(n,m+1):
-    for j in range(2,i):
-        if i%j!=0:
-            pass
-    else:
-        print
