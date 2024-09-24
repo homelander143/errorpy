a7f5df7bd815e2b7ca92d6c58a0790b773e2b30f
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:18:58 +0530

    Delete pythpro/2b.py

--- pythpro/2b.py
+++
@@ -1,8 +0,0 @@
-from math import *
-n=int(input("Enter the odd number of lines : "))
-for i in range(n):
-    print("*",end=" ",sep=" ")
-print()
-for j in range(int((n+1)/2)):
-    print("   ",end=" ",sep=" ")
-    print("*")
