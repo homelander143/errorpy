1517a513ef1b5ead4596fa972776099c209f3904
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:20:11 +0530

    Delete pythpro/4a.py

--- pythpro/4a.py
+++
@@ -1,7 +0,0 @@
-n=int(input("Enter the number of lines : "))
-for i in range(n+1):
-    for j in range(n-i):
-        print(" ",end=" ")
-    for j in range(i+1):
-        print("*  ",end=" ")
-    print()
