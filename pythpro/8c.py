5ac7b7fdce7019514495d6a0476d340a2e68d7d0
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:23:33 +0530

    Delete pythpro/8c.py

--- pythpro/8c.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the number : "))
-res=0
-for i in range(1,n):
-    if n%i==0:
-        res=res+i
-if res==n:
-    print(n," is perfect number")
-else:
-    print(n," is not perfect number")
