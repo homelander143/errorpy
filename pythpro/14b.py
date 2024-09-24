47b54da90e5d69305cd190a14e9bcd0474136261
homelander143 <vaibhavdlalasangi@gmail.com>
2024-09-25 05:12:20 +0530

    Delete pythpro/14b.py

--- pythpro/14b.py
+++
@@ -1,6 +0,0 @@
-li=[[2,5],[1,2],[4,4],[2,3],[2,1]]
-for i in range(len(li)-1):
-    for j in range(len(li)-i-1):
-        if li[j][-1]>li[j+1][-1]:
-            li[j],li[j+1]=li[j+1],li[j]
-print(li)
